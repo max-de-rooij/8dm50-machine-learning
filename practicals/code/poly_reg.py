82b84c401cee093f660ff1bcc84c5cf2c734d34f
max-de-rooij <m.d.rooij1@student.tue.nl>
2020-09-21 16:48:48 +0200

    week 2 af

--- practicals/code/poly_reg.py
+++ practicals/code/poly_reg.py
@@ -2,6 +2,6 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import make_pipeline
 
-def PolynomialRegression(degree=2, **kwargs):
+def PolynomialRegression(degree=2):
     return make_pipeline(PolynomialFeatures(degree),
-                         LinearRegression(**kwargs))
+                         LinearRegression())
